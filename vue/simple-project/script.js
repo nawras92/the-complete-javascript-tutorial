21ab24079ea9b42adbad95e3bc0afef002b6b0be
Nawras <nawras.othman@yandex.com>
2022-02-24 20:37:21 +0200

    Introduction to vuejs

---
+++ vue/simple-project/script.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+  data: function () {
+    return {
+      title: 'my beautiful project 2 ',
+      desc: 'this is my first vue projecttttttttttttttt',
+    };
+  },
+});
+
+app.mount('body');
