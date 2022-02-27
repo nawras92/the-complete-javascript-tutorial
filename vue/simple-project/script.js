70331159980acb5cd696c669df45c4f9c4a1c9f0
Nawras <nawras.othman@yandex.com>
2022-02-27 15:25:27 +0300

    VueJS episode 2: v-for

--- vue/simple-project/script.js
+++ vue/simple-project/script.js
@@ -1,8 +1,31 @@
+const sections = [
+  {
+    id: 1,
+    title: 'this is section 1',
+    desc: 'this is section 1 description',
+  },
+  {
+    id: 2,
+    title: 'this is section 2',
+    desc: 'this is section 2 description',
+  },
+  {
+    id: 3,
+    title: 'this is section 3',
+    desc: 'this is section 3 description',
+  },
+  {
+    id: 4,
+    title: 'this is section 4',
+    desc: 'this is section 4 description',
+  },
+];
 const app = Vue.createApp({
-  data: function () {
+  data() {
     return {
       title: 'my beautiful project 2 ',
       desc: 'this is my first vue projecttttttttttttttt',
+      sections,
     };
   },
 });
