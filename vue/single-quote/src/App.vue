d1081fddbbce4ce142ed6f9f96b0024140175b45
Nawras <nawras.othman@yandex.com>
2022-03-09 14:25:18 +0300

    VueJS episode 9: PageHeader component

---
+++ vue/single-quote/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <page-header title="My quotes" text="welcome to our website"></page-header>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin-bottom: 1rem;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue/single-quote/src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <h1 class="header-heading">{{ title }}</h1>
+    <p class="header-text">{{ text }}</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: ['title', 'text'],
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  padding: 1rem;
+}
+</style>
