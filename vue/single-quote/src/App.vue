08dfeb2520c2cf7031b894a521b4b0265929d4ea
Nawras <nawras.othman@yandex.com>
2022-03-13 19:20:04 +0300

    VueJS episode 10/11: SingleQuote Component/Read Data

--- vue/single-quote/src/App.vue
+++ vue/single-quote/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <page-header title="My quotes" text="welcome to our website"></page-header>
+  <div class="quotes">
+    <single-quote
+      v-for="quote in quotes"
+      :text="quote.quoteText"
+    ></single-quote>
+  </div>
 </template>
 
 <script>
 import PageHeader from './components/PageHeader.vue';
+import SingleQuote from './components/SingleQuote.vue';
+import quotes from '../quotes.json';
+console.log(quotes);
 export default {
   name: 'App',
   components: {
     PageHeader,
+    SingleQuote,
+  },
+  data() {
+    return {
+      quotes,
+    };
   },
 };
 </script>
@@ -33,5 +48,13 @@ p {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.quotes {
+  max-width: 80%;
+  margin: 1rem auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
 }
 </style>
---
+++ vue/single-quote/src/components/SingleQuote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="quote">
+    <p class="quote-text">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleQuote',
+  props: ['text'],
+};
+</script>
+
+<style scoped>
+.quote {
+  background: linear-gradient(30deg, #4bc0c8, #c7779d, #feac5e);
+  color: #333;
+  padding: 1rem;
+  border-radius: 10px;
+  margin: 1rem auto;
+}
+
+.quote-text {
+  font-size: 1rem;
+  font-weight: bold;
+}
+</style>
