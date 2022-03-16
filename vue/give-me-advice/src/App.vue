0d1f185df8692260898b767198b1620c288f5a6e
Nawras <nawras.othman@yandex.com>
2022-03-16 16:51:47 +0300

    VueJS episode 12/13: give me advice project

---
+++ vue/give-me-advice/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <page-header
+    title="Give me advice"
+    text="Provide a number and get a piece of advice"
+  >
+  </page-header>
+  <page-form></page-form>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+import PageForm from './components/PageForm.vue';
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    PageForm,
+  },
+};
+</script>
+
+<style>
+@import './assets/css/style.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue/give-me-advice/src/components/PageForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form class="form">
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Choose a number</legend>
+      <label class="form-label">Luck Number</label>
+      <input class="form-input" type="number" />
+    </fieldset>
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Text Color</legend>
+      <label class="form-label">Color</label>
+      <input class="form-input" type="color" />
+    </fieldset>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'PageForm',
+};
+</script>
+
+<style scoped>
+.form {
+  margin: 5rem auto;
+  max-width: 70%;
+}
+
+.form-fieldset {
+  margin: 2rem 0;
+  padding: 2rem;
+  border-radius: 10px;
+  background: linear-gradient(80deg, #e5e5e5, lightblue);
+}
+
+.form-legend {
+  display: block;
+  padding: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  background: linear-gradient(80deg, yellow, lightgreen);
+  border-radius: 10px;
+}
+
+.form-label {
+  display: block;
+  padding: 1rem;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.form-input {
+  outline: none;
+  border: none;
+  font-size: 2rem;
+  color: tomato;
+  text-align: center;
+  max-width: 30%;
+}
+</style>
