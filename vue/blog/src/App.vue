a4a836b0915c59871574ecab7fa0addba7673a04
Nawras <nawras.othman@yandex.com>
2022-04-11 16:56:11 +0300

    VueJS: Blog Project: Create Button Component

---
+++ vue/blog/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <blog-button title="Go there" background="crimson" color="#fff" />
+  <blog-button title="Go there" link="/blog" fullWidth />
+</template>
+
+<script>
+import BlogButton from './components/BlogButton.vue';
+export default {
+  name: 'App',
+  components: {
+    BlogButton,
+  },
+};
+</script>
+
+<style></style>
