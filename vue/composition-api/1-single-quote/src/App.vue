20a696e07490d9534fc4399eb49d811b968f5167
Nawras <nawras.othman@yandex.com>
2022-04-18 22:51:07 +0300

    Composition API: 1-Single Quote

---
+++ vue/composition-api/1-single-quote/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <page-header title="My quotes" text="welcome to our website"></page-header>
+  <div class="quotes">
+    <single-quote
+      v-for="quote in quotes"
+      :text="quote.quoteText"
+    ></single-quote>
+  </div>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue';
+import SingleQuote from './components/SingleQuote.vue';
+import quotes from '../quotes.json';
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    SingleQuote,
+  },
+  setup() {
+    return {
+      quotes,
+    };
+  },
+  /* data() { */
+  /*   return { */
+  /*     quotes, */
+  /*   }; */
+  /* }, */
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin-bottom: 1rem;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.quotes {
+  max-width: 80%;
+  margin: 1rem auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+}
+</style>
---
+++ vue/composition-api/1-single-quote/src/components/SingleQuote.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="quote">
+    <p class="quote-text">{{ text }}</p>
+    {{ textEdited }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingleQuote',
+  props: ['text'],
+  setup(props) {
+    return {
+      textEdited: props.text + ' edited',
+    };
+  },
+};
+</script>
+
+<style scoped>
+.quote {
+  background: linear-gradient(30deg, #4bc0c8, #c7779d, #feac5e);
+  color: #333;
+  padding: 1rem;
+  border-radius: 10px;
+  margin: 1rem auto;
+}
+
+.quote-text {
+  font-size: 1rem;
+  font-weight: bold;
+}
+</style>
