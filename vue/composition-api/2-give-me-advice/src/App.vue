bd20b2237426c61ad2830706856f5db8652dc19e
Nawras <nawras.othman@yandex.com>
2022-04-20 14:11:56 +0300

    Composition API: 2-Give-me-advice

---
+++ vue/composition-api/2-give-me-advice/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <page-header
+    title="Give me advice"
+    text="Provide a number and get a piece of advice"
+  >
+  </page-header>
+  <page-form></page-form>
+</template>
+
+<script>
+import { provide } from 'vue';
+import PageHeader from './components/PageHeader.vue';
+import PageForm from './components/PageForm.vue';
+import quotes from '../data.json';
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    PageForm,
+  },
+  setup() {
+    provide('quotes', quotes);
+  },
+  /* provide() { */
+  /*   return { */
+  /*     quotes: this.quotes, */
+  /*   }; */
+  /* }, */
+  /* data() { */
+  /*   return { */
+  /*     quotes, */
+  /*   }; */
+  /* }, */
+};
+</script>
+
+<style>
+@import './assets/css/style.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ vue/composition-api/2-give-me-advice/src/components/PageForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <form class="form">
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Choose a number</legend>
+      <label class="form-label">Luck Number</label>
+      <input class="form-input" type="number" v-model="userNumber" />
+    </fieldset>
+    <fieldset class="form-fieldset">
+      <legend class="form-legend">Text Color</legend>
+      <label class="form-label">Color</label>
+      <input class="form-input" type="color" v-model="userColor" />
+    </fieldset>
+    <form-result :color="userColor" :advice="getAdvice"></form-result>
+  </form>
+</template>
+
+<script>
+import { inject, computed, ref } from 'vue';
+import FormResult from './FormResult.vue';
+export default {
+  name: 'PageForm',
+  components: {
+    FormResult,
+  },
+  setup() {
+    const quotes = inject('quotes');
+    const userNumber = ref(5);
+    const userColor = ref('#000000');
+    console.log(userNumber);
+
+    const getAdvice = computed(() => {
+      const advice = quotes.find((q, i) => i === userNumber.value);
+
+      if (!advice) return 'unlucky enough, no advice for you';
+      return advice.quoteText;
+    });
+    return {
+      userColor,
+      userNumber,
+      getAdvice,
+    };
+  },
+
+  /* inject: ['quotes'], */
+  /* data() { */
+  /*   return { */
+  /*     userNumber: 5, */
+  /*     userColor: '#000000', */
+  /*   }; */
+  /* }, */
+  /* computed: { */
+  /*   getAdvice() { */
+  /*     const advice = this.quotes.find((q, i) => i === this.userNumber); */
+
+  /*     if (!advice) return 'unlucky enough, no advice for you'; */
+  /*     return advice.quoteText; */
+  /*   }, */
+  /* }, */
+};
+</script>
+
+<style scoped>
+.form {
+  margin: 5rem auto;
+  max-width: 70%;
+}
+
+.form-fieldset {
+  margin: 2rem 0;
+  padding: 2rem;
+  border-radius: 10px;
+  background: linear-gradient(80deg, #e5e5e5, lightblue);
+}
+
+.form-legend {
+  display: block;
+  padding: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+  background: linear-gradient(80deg, yellow, lightgreen);
+  border-radius: 10px;
+}
+
+.form-label {
+  display: block;
+  padding: 1rem;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.form-input {
+  outline: none;
+  border: none;
+  font-size: 2rem;
+  color: tomato;
+  text-align: center;
+  max-width: 30%;
+}
+</style>
