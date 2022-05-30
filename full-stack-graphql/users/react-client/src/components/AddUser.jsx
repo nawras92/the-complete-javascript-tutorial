d064579dabbea785d2527e716383f5c4f230e38a
Nawras <nawras.othman@yandex.com>
2022-05-30 11:54:24 +0300

    Full Stack GraphQL: Users App (14) - react - Add Home, Users, AddUser components

---
+++ full-stack-graphql/users/react-client/src/components/AddUser.jsx
@@ -0,0 +1,9 @@
+const AddUser = () => {
+  return (
+    <div>
+      <h1>You can add a user here</h1>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ full-stack-graphql/users/react-client/src/components/Users.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div>
+      <h1>Welcome to Users</h1>
+    </div>
+  );
+};
+
+export default Users;
