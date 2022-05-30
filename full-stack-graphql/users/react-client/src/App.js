dab70e7d8ee8b45435ceb96363b3af4906c0ca10
Nawras <nawras.othman@yandex.com>
2022-05-30 11:41:12 +0300

    Full Stack GraphQL: Users App (11) - react - Add nav

--- full-stack-graphql/users/react-client/src/App.js
+++ full-stack-graphql/users/react-client/src/App.js
@@ -6,17 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <a href="/" style={{ color: 'White' }}>
+            Home |
+          </a>
+          <a href="/users" style={{ color: 'White' }}>
+            Users |
+          </a>
+          <a href="/add-user" style={{ color: 'White' }}>
+            Add User |
+          </a>
+        </nav>
       </header>
     </div>
   );
