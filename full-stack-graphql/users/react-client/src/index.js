7db128ee1d4dc0bbfbad4604d64bd44188def187
Nawras <nawras.othman@yandex.com>
2022-05-30 11:33:40 +0300

    Full Stack GraphQL: Users App (10) - react - connect to Apollo

--- full-stack-graphql/users/react-client/src/index.js
+++ full-stack-graphql/users/react-client/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
