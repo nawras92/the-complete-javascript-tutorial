3117a496184e800be7c4a3cfc9e456cd3de4a228
Nawras <nawras.othman@yandex.com>
2022-06-18 15:55:08 +0300

    delete user

--- full-stack-graphql/users/vue-client/src/components/SingleUser.vue
+++ full-stack-graphql/users/vue-client/src/components/SingleUser.vue
@@ -8,13 +8,44 @@
       <p>is active: {{ user.isActive ? 'active' : 'NOT active' }}</p>
       <hr />
       <hr />
+      <button class="danger" @click="showDeleteSection = !showDeleteSection">
+        Delete
+      </button>
+      <button class="info" @click="showEditSection = !showEditSection">
+        Edit
+      </button>
+    </div>
+
+    <div class="delete-section" v-show="showDeleteSection">
+      <p>Are you sure you want to delete this?</p>
+      <button class="danger" @click="handleDeleteUser(user.id)">
+        yes delete it
+      </button>
+      <button class="info" @click="showDeleteSection = false">Cancel</button>
+      <p>{{ deleteDone }}</p>
+    </div>
+    <hr />
+    <hr />
+    <div class="edit-section" v-show="showEditSection">
+      <edit-user
+        :key="new Date().valueOf()"
+        :firstName="user.firstName"
+        :username="user.username"
+        :favNumber="user.favNumber"
+        :isActive="user.isActive"
+        :id="user.id"
+      ></edit-user>
     </div>
   </div>
 </template>
 
 <script>
 import { gql } from '@apollo/client/core';
+import EditUser from './EditUser.vue';
 export default {
+  components: {
+    EditUser,
+  },
   apollo: {
     user: {
       query: gql`
@@ -39,7 +70,50 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
+      showDeleteSection: false,
+      showEditSection: false,
+      user: {
+        firstName: '',
+        username: '',
+        favNumber: 0,
+        isActive: false,
+      },
+      deleteDone: '',
     };
+  },
+  methods: {
+    handleDeleteUser(id) {
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation deleteUserFunc($id: ID!) {
+            removeUser(id: $id)
+          }
+        `,
+        variables: {
+          id,
+        },
+      });
+      this.deleteDone = 'You have deleted the user successfully';
+      setTimeout(() => {
+        this.showDeleteSection = false;
+      }, 2000);
+    },
   },
 };
 </script>
+
+<style>
+.danger {
+  background: red;
+  color: white;
+  padding: 1rem;
+  margin: 1rem;
+}
+
+.info {
+  background: lightBlue;
+  color: #555;
+  padding: 1rem;
+  margin: 1rem;
+}
+</style>
