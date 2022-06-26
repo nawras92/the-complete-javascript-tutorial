5650415e98bb90631a0e0cec10f79b34e738fdc9
Nawras <nawras.othman@yandex.com>
2022-06-26 12:03:31 +0300

    establish db connection

---
+++ full-stack-graphql/users-with-database/server/models/db.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  'graphql', // Database Name
+  'root', // Database user
+  'mypass', // Database password,
+  {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: '3306',
+  }
+);
+
+async function testDBConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      'Connection to graphql database has been established successfully'
+    );
+  } catch (e) {
+    console.log(`Database connection error: ${e}`);
+  }
+}
+
+testDBConnection();
+
+module.exports = {
+  sequelize,
+};
