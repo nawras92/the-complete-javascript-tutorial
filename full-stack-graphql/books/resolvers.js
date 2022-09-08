9155da805e887f0d0200bedabc95b0d6dfd4d43d
Nawras <nawras.othman@yandex.com>
2022-09-08 15:11:59 +0300

    add query resolvers

--- full-stack-graphql/books/resolvers.js
+++ full-stack-graphql/books/resolvers.js
@@ -3,5 +3,17 @@ module.exports = {
     hello: () => {
       return 'Hello from graphql again';
     },
+
+    getBooks: (parent, args, context) => {
+      const { books } = context;
+      return books;
+    },
+
+    getBook: (parent, args, context) => {
+      const { books } = context;
+      const { id } = args;
+      const book = books.find((b) => b.id === id);
+      return book;
+    },
   },
 };
