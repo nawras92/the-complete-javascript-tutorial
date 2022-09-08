9c6a03038c54394646924f605aa239cd10e421da
Nawras <nawras.othman@yandex.com>
2022-09-08 15:11:47 +0300

    add query typeDefs

--- full-stack-graphql/books/typeDefs.js
+++ full-stack-graphql/books/typeDefs.js
@@ -3,5 +3,14 @@ const { gql } = require('apollo-server-express');
 module.exports = gql`
   type Query {
     hello: String
+    getBooks: [Book]
+    getBook(id: ID!): Book
+  }
+
+  type Book {
+    id: ID!
+    title: String!
+    description: String
+    authorId: String
   }
 `;
