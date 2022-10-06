9077d663ee3c29b7b11a2a5bb4637554fd7c0a48
Nawras <nawras.othman@yandex.com>
2022-10-06 10:45:26 +0300

    add mutation resolvers

--- full-stack-graphql/books/resolvers.js
+++ full-stack-graphql/books/resolvers.js
@@ -16,4 +16,32 @@ module.exports = {
       return book;
     },
   },
+  Mutation: {
+    addBook: (parent, args, context) => {
+      const { books } = context;
+      const { id, title, description, authorId } = args;
+      books.push({ id, title, description, authorId });
+
+      return true;
+    },
+    deleteBook: (parent, args, context) => {
+      const { books } = context;
+      const { id } = args;
+      const theBookIndex = books.findIndex((book) => book.id === id);
+      books.splice(theBookIndex, 1);
+      return true;
+    },
+    editBook: (parent, args, context) => {
+      const { books } = context;
+      const { id, title, description, authorId } = args;
+      const theBook = books.find((book) => book.id === id);
+      if (theBook) {
+        theBook.title = title;
+        theBook.description = description || theBook.description;
+        theBook.authorId = authorId || theBook.authorId;
+        return true;
+      }
+      return false;
+    },
+  },
 };
