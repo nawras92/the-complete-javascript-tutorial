df1dddbc61cc227ae72512c308c86f6c873aadea
Nawras <nawras.othman@yandex.com>
2023-05-19 00:35:38 +0300

    show recipes based on logged In user

--- next/7-recipe-project/pages/api/my-recipes.js
+++ next/7-recipe-project/pages/api/my-recipes.js
@@ -1,10 +1,10 @@
 import { getMyRecipes } from '../../api/recipe';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
     try {
       // get User Id
-      const userId = 2;
+      const { userId } = JSON.parse(req.body);
       // Send Request to Backend
       const response = await fetch(getMyRecipes(userId));
       const returnedData = await response.json();
@@ -20,10 +20,14 @@ export default async function handler(req, res) {
 
       // response is ok
       const { data } = returnedData;
+      const recipes = data.map((r) => ({
+        id: r.id,
+        ...r.attributes,
+      }));
       res.status(200);
       res.json({
         ok: true,
-        recipes: data,
+        recipes,
       });
     } catch (e) {
       console.log(e);
--- next/7-recipe-project/pages/dashboard/recipes.js
+++ next/7-recipe-project/pages/dashboard/recipes.js
@@ -1,6 +1,39 @@
 import styles from '../../styles/dashboard.module.css';
 
 export default function MyRecipesPage(props) {
+  const { recipes } = props;
+
+  const handleDelete = async (id) => {
+    const result = window.confirm(
+      'Are you sure you want to delete this recipe?'
+    );
+    if (result) {
+      try {
+        // Send delete request to the backend
+        const response = await fetch('/api/deleteRecipe', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id }),
+        });
+        const returnedData = await response.json();
+        const { ok, message } = returnedData;
+        if (!ok) {
+          throw new Error(message);
+        }
+        //Success
+        // reload page on delete
+        window.location.reload();
+
+        console.log(message);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    //error
+  };
   return (
     <div className={styles['page-container']}>
       <div className={styles['page-header']}>
@@ -17,33 +50,69 @@ export default function MyRecipesPage(props) {
               <th>X</th>
             </tr>
           </thead>
+
           <tbody>
-            <tr>
-              <td>#1</td>
-              <td>
-                <a href="/id">My title</a>
-              </td>
-              <td>my description</td>
-              <td>
-                <a
-                  href="/edit/id"
-                  className={`${styles['button']} ${styles['button-edit']}`}
-                >
-                  Edit
-                </a>
-              </td>
-              <td>
-                <a
-                  onClick=""
-                  className={`${styles['button']} ${styles['button-delete']}`}
-                >
-                  Delete
-                </a>
-              </td>
-            </tr>
+            {recipes &&
+              recipes.map((r, i) => {
+                return (
+                  <tr key={i}>
+                    <td>#{r.id}</td>
+                    <td>
+                      <a href={`/${r.id}`}>{r.title}</a>
+                    </td>
+                    <td>{r.description}</td>
+                    <td>
+                      <a
+                        href={`/edit/${r.id}`}
+                        className={`${styles['button']} ${styles['button-edit']}`}
+                      >
+                        Edit
+                      </a>
+                    </td>
+                    <td>
+                      <a
+                        onClick={() => handleDelete(r.id)}
+                        className={`${styles['button']} ${styles['button-delete']}`}
+                      >
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const url = 'http://localhost:3000/api/my-recipes';
+    // Get User Id
+    const { req } = context;
+    const data = req.cookies['LWNUser'];
+    const { id } = JSON.parse(data);
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ userId: id }),
+    });
+    if (!response.ok) {
+      throw new Error('Error in requesting api/my-recipes');
+    }
+    const { recipes } = await response.json();
+    return {
+      props: {
+        recipes,
+      },
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      props: {
+        recipes: null,
+      },
+    };
+  }
 }
