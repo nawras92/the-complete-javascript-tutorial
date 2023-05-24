eb77f46b51ddfab1b9c79ae55fcde7bffef0ca38
Nawras <nawras.othman@yandex.com>
2023-05-24 12:56:50 +0300

    apply messages and layout to dashboard

--- next/7-recipe-project/pages/dashboard/index.js
+++ next/7-recipe-project/pages/dashboard/index.js
@@ -1,35 +1,43 @@
 import styles from '../../styles/dashboard.module.css';
 import useUser from '../../hooks/useUser';
+import Layout from '../../components/Layout';
+import { dashboard_page_title } from '../../messages';
+import { dashboard_page_welcome_message } from '../../messages';
+import { nav_link_add_recipe } from '../../messages';
+import { nav_link_my_recipes } from '../../messages';
+import { nav_link_all_recipes } from '../../messages';
 
 export default function DashboardPage() {
   useUser({ redirectTo: true });
   return (
-    <div className={styles['dashboard-container']}>
-      <div className={styles['dashboard']}>
-        <h1 className={styles['dashboard-welcome']}>
-          Welcome to Recipe Dashboard
-        </h1>
-        <div className={styles['button-group']}>
-          <a
-            href="/dashboard/recipes"
-            className={`${styles['button']} ${styles['button-warning']}`}
-          >
-            My Recipes
-          </a>
-          <a
-            href="/"
-            className={`${styles['button']} ${styles['button-info']}`}
-          >
-            All Recipes
-          </a>
-          <a
-            href="/add"
-            className={`${styles['button']} ${styles['button-success']}`}
-          >
-            Add Recipe
-          </a>
-        </div>{' '}
+    <Layout title={dashboard_page_title}>
+      <div className={styles['dashboard-container']}>
+        <div className={styles['dashboard']}>
+          <h1 className={styles['dashboard-welcome']}>
+            {dashboard_page_welcome_message}
+          </h1>
+          <div className={styles['button-group']}>
+            <a
+              href="/dashboard/recipes"
+              className={`${styles['button']} ${styles['button-warning']}`}
+            >
+              {nav_link_my_recipes}
+            </a>
+            <a
+              href="/"
+              className={`${styles['button']} ${styles['button-info']}`}
+            >
+              {nav_link_all_recipes}
+            </a>
+            <a
+              href="/add"
+              className={`${styles['button']} ${styles['button-success']}`}
+            >
+              {nav_link_add_recipe}
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
