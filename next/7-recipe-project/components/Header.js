1373d58fa74d2d57861b9dc7eaf509cbe16336c4
Nawras <nawras.othman@yandex.com>
2023-05-24 12:43:09 +0300

    apply messages to header

--- next/7-recipe-project/components/Header.js
+++ next/7-recipe-project/components/Header.js
@@ -1,6 +1,13 @@
 import { useRouter } from 'next/router';
 import styles from '../styles/header.module.css';
 import useUser from '../hooks/useUser';
+import { nav_link_homepage } from '../messages';
+import { nav_link_login } from '../messages';
+import { nav_link_register } from '../messages';
+import { nav_link_dashboard } from '../messages';
+import { nav_link_logout } from '../messages';
+import { nav_link_add_recipe } from '../messages';
+import { nav_link_my_recipes } from '../messages';
 
 export default function Header() {
   const user = useUser();
@@ -24,16 +31,16 @@ function NonLoggedInHeader() {
       <ul className={styles['header-nav']}>
         <li className={styles['header-nav-item']}>
           <a className={styles['header-nav-link']} href="/">
-            Home
+            {nav_link_homepage}
           </a>
         </li>
       </ul>
       <div className={styles['header-admin']}>
         <a className={styles['button']} href="/login">
-          Login
+          {nav_link_login}
         </a>
         <a className={styles['button']} href="/register">
-          Register
+          {nav_link_register}
         </a>
       </div>
     </header>
@@ -48,26 +55,26 @@ function LoggedInHeader(props) {
       <ul className={styles['header-nav']}>
         <li className={styles['header-nav-item']}>
           <a className={styles['header-nav-link']} href="/">
-            Home
+            {nav_link_homepage}
           </a>
         </li>
         <li className={styles['header-nav-item']}>
           <a className={styles['header-nav-link']} href="/add">
-            Add Recipe
+            {nav_link_add_recipe}
           </a>
         </li>
         <li className={styles['header-nav-item']}>
           <a className={styles['header-nav-link']} href="/dashboard/recipes">
-            My Recipes
+            {nav_link_my_recipes}
           </a>
         </li>
       </ul>
       <div className={styles['header-admin']}>
         <a className={styles['button']} href="/dashboard">
-          Dashboard
+          {nav_link_dashboard}
         </a>
         <button className={styles['button']} onClick={handleLogout}>
-          Logout
+          {nav_link_logout}
         </button>
       </div>
     </header>
