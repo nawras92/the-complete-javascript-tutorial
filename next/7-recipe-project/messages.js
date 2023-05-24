91d3eb99b16dfc074475e94c445d136f7a56ca3a
Nawras <nawras.othman@yandex.com>
2023-05-24 10:53:59 +0300

    add messages

---
+++ next/7-recipe-project/messages.js
@@ -0,0 +1,80 @@
+/* Pages Title */
+export const homepage_title = 'أشهى الوصفات لدينا';
+export const dashboard_page_title = 'لوحة تحكم المستخدم';
+export const my_recipes_page_title = 'إدارة وصفاتي';
+export const my_recipes_page_recipe_title = 'عنوان الوصفة';
+export const my_recipes_page_recipe_description = 'وصف الوصفة';
+export const my_recipes_page_recipe_id = 'معرّف الوصفة';
+export const my_recipes_page_recipe_edit = 'تعديل الوصفة';
+export const my_recipes_page_recipe_delete = 'حذف الوصفة';
+export const my_recipes_page_recipe_delete_prompt = 'هل تريد حذف هذه الوصفة؟';
+/* Dashboard Pages */
+export const dashboard_page_welcome_message =
+  'أهلا بكم في لوحة التحكم بالوصفات';
+/* Nav Links */
+export const nav_link_homepage = 'الرئيسية';
+export const nav_link_login = 'تسجيل الدخول';
+export const nav_link_register = 'إنشاء حساب';
+export const nav_link_dashboard = 'لوحة التحكم';
+export const nav_link_logout = 'تسجيل الخروج';
+export const nav_link_add_recipe = 'إضافة وصفة جديدة';
+export const nav_link_my_recipes = 'وصفاتي';
+export const nav_link_all_recipes = 'كل الوصفات';
+/*Authentication Pages: LOGIN / REGISTER */
+export const login_page_title = 'تسجيل الدخول في موقع أشهى الوصفات';
+export const register_page_title = 'إنشاء حساب في موقع أشهى الوصفات';
+export const authentication_page_email_label = 'البريد الإلكتروني';
+export const authentication_page_username_label = 'اسم المستخدم';
+export const authentication_page_password_label = 'كلمة المرور';
+export const authentication_page_login_button = 'تسجيل الدخول';
+export const authentication_page_register_button = 'إنشاء حساب';
+/*Recipes Related */
+export const recipes_updated_at = 'تاريخ التحديث';
+export const recipes_meal = 'الوجبة';
+export const recipes_servings = 'عدد الأشخاص';
+export const recipes_cook_time = 'مدة الطهي';
+export const recipes_preparation_time = 'مدة التحضير';
+export const recipes_total_time = 'المدة الكلية';
+export const recipes_ingredients = 'المقادير';
+export const recipes_steps = 'الخطوات';
+export const recipes_notes = 'الملاحظات';
+/*Recipe Form*/
+export const recipe_form_add_message = 'تم إضافة الوصفة بنجاح!';
+export const recipe_form_edit_message = 'تم تعديل الوصفة بنجاح!';
+export const add_recipe_page_title = 'إضافة وصفة جديدة';
+export const edit_recipe_page_title = 'تعديل الوصفة';
+export const recipe_title_field = 'اسم الوصفة';
+export const recipe_title_helper_text =
+  'اسم الوصفة التي تريد إضافتها. يجب أن يتكون اسم الوصفة من 5  - 60 محرف.';
+export const recipe_description_field = 'الوصف';
+export const recipe_description_helper_text =
+  'وصف قصير عن الوصفة. يجب أن يتكون الوصف من 10 - 160 محرف. ';
+export const recipe_steps_field = 'الخطوات';
+export const recipe_steps_helper_text =
+  'خطوات واضحة ومرتبة حول كيفية إعداد هذه الوصفة.';
+export const recipe_ingredients_field = 'المكونات';
+export const recipe_ingredients_helper_text =
+  'مقادير الوصفة مع أسماء الوصفة بشكل واضح.';
+export const recipe_notes_field = 'ملاحظات';
+export const recipe_notes_helper_text =
+  'إذا كان لديكم أي ملاحظات إضافية لمعدّ الوصفة أو القارىء، الرجاء إضافتها في حقل الملاحظات.';
+export const recipe_cook_time_field = 'مدة الطهي';
+export const recipe_cook_time_helper_text =
+  'المدة اللازمة لطهي هذه الوصفة. ادخل المدة بالدقائق.';
+export const recipe_preparation_time_field = 'مدة التحضير';
+export const recipe_preparation_time_helper_text =
+  'المدة اللازمة لتحضير هذه الوصفة. ادخل المدة بالدقائق.';
+export const recipe_total_time_field = 'المدة الكلية';
+export const recipe_total_time_helper_text =
+  'المدة الكلية لتحضير وطهي الوصفة. ادخل المدة بالدقائق.';
+export const recipe_servings_field = 'عدد الأشخاص';
+export const recipe_servings_helper_text =
+  'هذه الوصفة بهذه المقادير تكفي كم شخص؟';
+export const recipe_meal_field = 'الوجبة';
+export const recipe_meal_helper_text =
+  'هذه الوصفة تقدم بأي وجبة من اليوم: الفطور، الغذاء أم العشاء؟';
+export const recipe_submit_add = 'إضافة الوصفة';
+export const recipe_submit_edit = 'تعديل الوصفة';
+export const recipe_meal_breakfast = 'الإفطار';
+export const recipe_meal_lunch = 'الغذاء';
+export const recipe_meal_dinner = 'العشاء';
