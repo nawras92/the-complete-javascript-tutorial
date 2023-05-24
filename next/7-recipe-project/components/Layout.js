3c321e1472542207d5164fe8cbd9a41074f02154
Nawras <nawras.othman@yandex.com>
2023-05-24 11:03:52 +0300

    Add changa font

--- next/7-recipe-project/components/Layout.js
+++ next/7-recipe-project/components/Layout.js
@@ -1,11 +1,18 @@
 import Head from 'next/head';
 import Header from './Header';
+import { Changa } from 'next/font/google';
 import PageHeader from './PageHeader';
+
+const changa = Changa({
+  variable: '--changa-font',
+  subsets: ['arabic'],
+  weight: ['200', '300', '500', '700', '800'],
+});
 
 export default function Layout(props) {
   const { title, description = 'page description', children } = props;
   return (
-    <div>
+    <div className={changa.className}>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
