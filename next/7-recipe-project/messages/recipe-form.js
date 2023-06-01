f692758f40e639243e9adeeea592c1376dcd8640
Nawras <nawras.othman@yandex.com>
2023-06-01 10:49:30 +0300

    refactor recipe form messages

---
+++ next/7-recipe-project/messages/recipe-form.js
@@ -0,0 +1,44 @@
+const messages = {
+  add_message: 'تم إضافة الوصفة بنجاح!',
+  edit_message: 'تم تعديل الوصفة بنجاح!',
+  title_field: 'اسم الوصفة',
+  title_helper_text:
+    'اسم الوصفة التي تريد إضافتها. يجب أن يتكون اسم الوصفة من 5  - 60 محرف.',
+  description_field: 'الوصف',
+  description_helper_text:
+    'وصف قصير عن الوصفة. يجب أن يتكون الوصف من 10 - 160 محرف. ',
+  thumbnail_field: 'الصورة البارزة',
+  thumbnail_helper_text:
+    'ارفع الصورة البازرة للوصفة هنا بالنقر على زر رفع الصورة. قياس الصورة يجب أن يكون 200 * 200',
+  thumbnail_upload: 'رفع الصورة',
+  thumbnail_delete: 'حذف الصورة',
+  new_thumbnail:
+    'معاينة الصورة التي اخترتها. انقر على زر رفع الصورة لإرسالها للسيرفر',
+  steps_field: 'الخطوات',
+  steps_helper_text: 'خطوات واضحة ومرتبة حول كيفية إعداد هذه الوصفة.',
+  ingredients_field: 'المكونات',
+  ingredients_helper_text: 'مقادير الوصفة مع أسماء الوصفة بشكل واضح.',
+  notes_field: 'ملاحظات',
+  notes_helper_text:
+    'إذا كان لديكم أي ملاحظات إضافية لمعدّ الوصفة أو القارىء، الرجاء إضافتها في حقل الملاحظات.',
+  cook_time_field: 'مدة الطهي',
+  cook_time_helper_text: 'المدة اللازمة لطهي هذه الوصفة. ادخل المدة بالدقائق.',
+  preparation_time_field: 'مدة التحضير',
+  preparation_time_helper_text:
+    'المدة اللازمة لتحضير هذه الوصفة. ادخل المدة بالدقائق.',
+  total_time_field: 'المدة الكلية',
+  total_time_helper_text:
+    'المدة الكلية لتحضير وطهي الوصفة. ادخل المدة بالدقائق.',
+  servings_field: 'عدد الأشخاص',
+  servings_helper_text: 'هذه الوصفة بهذه المقادير تكفي كم شخص؟',
+  meal_field: 'الوجبة',
+  meal_helper_text:
+    'هذه الوصفة تقدم بأي وجبة من اليوم: الفطور، الغذاء أم العشاء؟',
+  submit_add: 'إضافة الوصفة',
+  submit_edit: 'تعديل الوصفة',
+  meal_breakfast: 'الإفطار',
+  meal_lunch: 'الغذاء',
+  meal_dinner: 'العشاء',
+};
+
+export default messages;
